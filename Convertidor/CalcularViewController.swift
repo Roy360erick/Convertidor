b9d5cae81c4cdd66010a7d98f1e55cd2c3795ec6
roy360erick <erick.fernandez@tecsup.edu.pe>
2019-05-07 18:25:13 -0500

    second commit

--- Convertidor/CalcularViewController.swift
+++ Convertidor/CalcularViewController.swift
@@ -10,26 +10,56 @@ import UIKit
 
 class CalcularViewController: UIViewController {
 
+    @IBOutlet weak var medida: UITextField!
+    @IBOutlet weak var unidadActual: UITextField!
+    @IBOutlet weak var unidadAConvertir: UITextField!
+    @IBOutlet weak var respuesta: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func calcular(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        var rpta : Double? = 0
+        
+        if unidadActual.text == "mi" && unidadAConvertir.text == "yd" {
+            //Millas a Yardas
+            rpta = Double(medida.text!)! * 1760
+            respuesta.text = "\( rpta! ) yardas"
+        }else if unidadActual.text == "mi" && unidadAConvertir.text == "km"{
+            //Milas a Kilometros
+            rpta = Double(medida.text!)! / 0.62137
+            respuesta.text = "\(rpta! ) Kilometros"
+        }else if unidadActual.text == "yd" && unidadAConvertir.text == "mi"{
+            //Yardas a Millas
+            rpta = Double(medida.text!)! * 0.00056818
+            respuesta.text = "\( rpta! ) Millas"
+        }else if unidadActual.text == "yd" && unidadAConvertir.text == "km"{
+            //Yardas a Kilometros
+            rpta = Double(medida.text!)! / 1093.6
+            respuesta.text = "\( rpta! ) Kilometros"
+        }else if unidadActual.text == "km" && unidadAConvertir.text == "yd"{
+            //Kilometros a Yardas
+            rpta = Double(medida.text!)! * 1093.61
+            respuesta.text = "\( rpta! ) yardas"
+        }else if unidadActual.text == "km" && unidadAConvertir.text == "mi"{
+            //Kilometros a Millas
+            rpta = Double(medida.text!)! * 0.62137
+            respuesta.text = "\( rpta! ) Millas"
+        }
+        
+        let conversion = Conversion(context: context)
+        conversion.medida = Double(medida.text!)!
+        conversion.unidadInicial = unidadActual.text!
+        conversion.unidadFinal = unidadAConvertir.text!
+        conversion.medidaFinal = rpta!
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
     }
-    */
-
+    
 }
--- Convertidor/ViewController.swift
+++ Convertidor/ViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var conversiones :[Conversion] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return conversiones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let conversion = conversiones[indexPath.row]
+        cell.textLabel?.text = "\( conversion.medida ) \( conversion.unidadInicial! ) Convertido a  \( conversion.medidaFinal ) \( conversion.unidadFinal! )"
+        return cell
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            conversiones = try context.fetch(Conversion.fetchRequest())
+            tableView.reloadData()
+        }catch{}
     }
 
 
